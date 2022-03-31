10b3db235911db76a1be78985c5875a080676705
Manoj Ponugtoi <jameslucky917@gmail.com>
2022-03-31 16:39:54 +0530

    a

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,28 +1,15 @@
 import React, { createContext, useState } from 'react';
 import ComponentA from "./ComponentA"
 
-const FirstName = createContext();
+
 
 const App = () => {
 
-    const [data, setData] = useState([
-        {
-            brandname: "Nokia"
-        },
-        {
-            brandname: "realme"
-        },
-        {
-            brandname: "mi"
-        }
-    ]);
-
-
 
     return (
-        <FirstName.Provider value={[data, setData]}>
-            <ComponentA />
-        </FirstName.Provider>
+        <>
+
+        </>
     );
 }
 
