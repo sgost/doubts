b4820fdc9e9111e397ad6daf44fb6ed478aa2185
Manoj Ponugtoi <jameslucky917@gmail.com>
2022-04-03 14:23:44 +0530

    a

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,29 @@
-import React, { Fragment, useState, useCallback } from "react"
+import React, { Fragment, useState, useEffect, useReducer } from "react"
+
+const initialState = { count: 0 };
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    default:
+      throw new Error()
+  }
+}
 
 const IndexPage = () => {
 
-  //useRef
-  const refCont = React.useRef("");
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  console.log(refCont.current.value)
-  ///////////////////////////////////////////////////////////
-  const [count, setCount] = useState(0);
-  //useCallback
-  useCallback(() => {
-    alert('useCallback')
-    fun();
-  }, [count])
-
-  const fun = () => {
-    alert('fun')
-  }
-
-  const ob = { a: 1, b: 2 };
   return (
-    <Fragment>
-      <input type="text" ref={refCont} />
-      <button onClick={() => setCount(count + 1)}>click</button>
-    </Fragment>
+    <>
+      {state.count}
+      <button onClick={() => dispatch({ type: 'decrement' })}>click</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>click</button>
+    </>
   )
 }
 
